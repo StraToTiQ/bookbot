20abdea6fe088f641340879fdac9f49263bb8caa
StraToTiQ <artur0griban123@gmail.com>
2025-11-03 18:40:16 +0100

    add a stats.py for analising fun..s; add import for main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def get_book_text(book_path):
 
     with open(book_path) as f:
@@ -5,14 +7,10 @@ def get_book_text(book_path):
 
     return book_contents
 
-def count_words(book_contents):
-    num_words = len(book_contents.split())
-    return num_words
-
 def main():
     book_path = "books/frankenstein.txt"
     book_contents = get_book_text(book_path)
-    num_words = count_words(book_contents)
+    num_words = get_num_words(book_contents)
     print(f"Found {num_words} total words")
     return
 
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_contents):
+    num_words = len(book_contents.split())
+    return num_words
