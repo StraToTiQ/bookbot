6f53f437baae90251161ce629d0314f1e51f8671
StraToTiQ <artur0griban123@gmail.com>
2025-11-03 18:52:51 +0100

    add count chars number in book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def get_book_text(book_path):
 
@@ -11,7 +12,9 @@ def main():
     book_path = "books/frankenstein.txt"
     book_contents = get_book_text(book_path)
     num_words = get_num_words(book_contents)
+    num_chars = get_num_chars(book_contents)
     print(f"Found {num_words} total words")
+    print(num_chars)
     return
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(book_contents):
     num_words = len(book_contents.split())
     return num_words
+
+def get_num_chars(book_contents):
+    book_content = book_contents.lower()
+    num_chars = {}
+    for ch in book_content:
+        if num_chars.get(ch) == None:
+            num_chars[ch] = 1
+        num_chars[ch] += 1
+    return num_chars
