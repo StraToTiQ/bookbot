c852e3584654990149b6a0b966a15a40573ed243
StraToTiQ <artur0griban123@gmail.com>
2025-11-03 19:27:04 +0100

    add sorting by num of char fun; add pring report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import get_sorted_list_num_chars
 
 def get_book_text(book_path):
 
@@ -13,8 +14,17 @@ def main():
     book_contents = get_book_text(book_path)
     num_words = get_num_words(book_contents)
     num_chars = get_num_chars(book_contents)
+    sorted_list_num_chars = get_sorted_list_num_chars(num_chars)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
+    print("--------- Character Count -------")
+    for item in sorted_list_num_chars:
+        if item["char"].isalpha() == True:
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
     return
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,13 @@ def get_num_chars(book_contents):
     for ch in book_content:
         num_chars[ch] = num_chars.get(ch, 0) + 1
     return num_chars
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_list_num_chars (num_chars):
+    list_chars = []
+    for key in num_chars:
+        list_chars.append({"char": key, "num": num_chars[key]})
+    list_chars.sort(reverse=True, key=sort_on)
+    return list_chars
